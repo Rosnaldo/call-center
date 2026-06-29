7ce823f054ccb7a6836246dc1b63514d5bf0a43c
Andrey Kenji Tsuzuki <andreytsuzuki@gmail.com>
2026-06-29 09:06:23 -0300

    fix domain

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -37,7 +37,7 @@ export default defineConfig(({ mode }) => {
       host: true,
       port: 5173,
       allowedHosts: [
-        "free-porn-block.net",
+        "free-porn-block.tech",
         "free-porn-block.com",
         "localhost",
         "127.0.0.1"
