ef731bf135af109d19c4a4728a41895946dd3c55
Andrey Kenji Tsuzuki <andreytsuzuki@gmail.com>
2026-07-04 14:56:39 -0300

    small logger and fix

--- apps/iam/src/const/collection_name_mapping.ts
+++ apps/iam/src/const/collection_name_mapping.ts
@@ -1,11 +1,9 @@
 export const userCollectionName = 'users';
 export const migrationCollectionName = 'migrations';
-export const callCollectionName = 'calls';
 export const callHistoryCollectionName = 'call_history';
 
 export const CollectionsName = [
     userCollectionName,
     migrationCollectionName,
-    callCollectionName,
     callHistoryCollectionName,
 ];
--- apps/realtime/src/websocket/broadcast.ts
+++ apps/realtime/src/websocket/broadcast.ts
@@ -1,11 +1,13 @@
 import { SOCKET_OPEN } from '#websocket/socket';
 import { WsMessage } from '#websocket/types';
 import { clientRegistry } from '#websocket/client_registry';
+import logger from '#logger';
 
 export const broadcastMessage = (payload: WsMessage<unknown>): void => {
     const message = JSON.stringify(payload);
     for (const client of clientRegistry) {
         if (client.readyState === SOCKET_OPEN) {
+            logger.info(`${ message } - broadcasting message to client`);
             client.send(message);
         }
     }
@@ -15,6 +17,7 @@ export const sendToUser = (userId: string, payload: WsMessage<unknown>): void =>
     const message = JSON.stringify(payload);
     for (const client of clientRegistry) {
         if (client.user?._id === userId && client.readyState === SOCKET_OPEN) {
+            logger.info(`${ message } - sending message to user: ${ userId }`);
             client.send(message);
         }
     }
